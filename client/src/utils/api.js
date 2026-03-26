173e0251344670ed10a68979fa6c4208b1d622de
veda shiva <vedashivaprasad97@gmail.com>
2026-03-26 14:18:41 +0530

    Update API URL to production backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.REACT_APP_API_URL || "https://splitmate-0a2j.onrender.com/api",
 });
 
 // Add token to all requests
